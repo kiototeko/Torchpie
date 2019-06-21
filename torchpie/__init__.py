c55d666a6d54da152e6999f1c8a0a3ca3db5f5a2
SunDoge <384813529@qq.com>
2019-06-21 16:49:37 +0800

    difficault

--- torchpie/__init__.py
+++ torchpie/__init__.py
@@ -8,7 +8,7 @@ __version__ = '0.2.0'
 # 这些变量直接用tp.[name]访问，挂在tp上以免污染全局变量。
 local_rank: int = args.local_rank
 debug: bool = args.debug
-experiment_path: Optional[str] = experiment_path
+# experiment_path: Optional[str] = experiment_path
 
 distributed: bool = is_distributed()
 
---
+++ torchpie/logging/functional.py
@@ -0,0 +1,5 @@
+def do_nothing(*args, **kwargs):
+    """
+    This function eats all your args and do nothing.
+    """
+    pass
