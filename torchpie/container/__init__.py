0298d2bf65daa25090799a19a2236b0167e01711
SunDoge <384813529@qq.com>
2019-07-10 20:16:35 +0800

    add rank0 logger

--- torchpie/container/__init__.py
+++ torchpie/container/__init__.py
@@ -2,11 +2,13 @@ from injector import Injector
 # from torchpie.argument.base import configure_args
 from ..core.environment import ArgsModule
 from ..core.config import ConfigModule
+from ..core.logging import LoggerModule
 
 # 在这里注册
 modules = [
     ArgsModule(),
     ConfigModule(),
+    LoggerModule(),
 ]
 
 container = Injector(modules=modules)
--- torchpie/core/logging/__init__.py
+++ torchpie/core/logging/__init__.py
@@ -2,12 +2,45 @@ import logging
 from injector import Module, singleton, provider
 from torchpie.parallel import rank0_wrapper
 from ..environment import Args
+from torchpie.utils import get_timestamp
+import sys
+import os
 
 LOG_FORMAT = '%(asctime)s|%(levelname)-8s|%(filename)s:%(lineno)d %(message)s'
 
+
 class LoggerModule(Module):
 
-    
+    @singleton
+    @provider
     def provide_logger(self, args: Args) -> logging.Logger:
+
+        logger = logging.getLogger('torchpie')
+
+        log_format = '%(asctime)s|%(levelname)-8s|%(filename)s:%(lineno)d %(message)s'
+        formatter = logging.Formatter(log_format)
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+        if args.experiment_path is not None:
+            # Add timestamp to filename
+            file_handler = logging.FileHandler(
+                os.path.join(args.experiment_path, f'result_{get_timestamp()}.log'))
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+        else:
+            logger.warning(
+                'No experiment path, no log file will be generated.')
+
+        if args.debug:
+            logger.setLevel(logging.DEBUG)
+        else:
+            logger.setLevel(logging.INFO)
+
+        logger.info('Logger for local_rank={}'.format(args.local_rank))
+
         
-        logger = None
+
+        return logger
